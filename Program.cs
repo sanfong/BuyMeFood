3a2fde1827317c77a274aca119c81896f1b37595
wikimaru <64010892@kmitl.ac.th>
2023-04-09 14:34:41 +0700

    HelloTest

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
-//hell lol2
+//AHHHHHHHHHHHH
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 

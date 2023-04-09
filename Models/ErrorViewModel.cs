96d521653c9702f80ec901ce31f352dede497fb0
Apinyawat Khwanpruk <sanfong2222@gmail.com>
2023-04-09 14:17:44 +0700

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BuyMeFood.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

7ed8310298fd64b7289e0258bfd901a1b187cde9
Apinyawat Khwanpruk <sanfong2222@gmail.com>
2023-04-19 00:44:15 +0700

    RegisterModel

---
+++ Models/RegisterModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuyMeFood.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        public string? Username { get; set; }
+        [Required]
+        [MinLength(6)]
+        public string? Password { get; set; }
+        [Required]
+        [Compare("Password")]
+        public string? ConfirmPassword { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? FullName { get; set; }
+        [Required]
+        [Phone]
+        public string? PhoneNumber { get; set; }
+    }
+}

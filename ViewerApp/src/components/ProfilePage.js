118676f8acf66a9996f6d3c64b70defe6d6717c6
James <tanakrit.thum@gmail.com>
2023-04-28 18:45:25 +0700

    add convertImgToBase64.css

--- ViewerApp/src/components/ProfilePage.js
+++ ViewerApp/src/components/ProfilePage.js
@@ -1,47 +1,33 @@
 ﻿import React from 'react';
+import './ProfilePage.css';
+import { FaPhoneAlt } from 'react-icons/fa';
+import { FiCreditCard } from 'react-icons/fi';
+//ถ้าเปิดไม่ได้แสดงว่ายังไม่ได้ติดตั้ง npm install react-icons//
 
 const ProfilePage = (props) => {
-    const { img,name,fullname,tel,bcredit,ccredit}= props;
+    const { img, name, fullname, tel, bcredit, ccredit } = props;
 
     return (
-        <div style={{
-            display: 'flex',
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-            margin: '20px',
-            borderRadius: '5px',
-            padding: '5%',
-}}>
-            <div style={{
-                flex: '1', 
-                display: 'flex', 
-                justifyContent: 'center', 
-                alignItems: 'center', 
-                marginRight: '20px',
-            }}>
-                <div style={{
-                    width: '14rem',
-                    borderRadius: '100%',
-                    overflow: 'hidden',
-                    }}>
-                    <img src={img} style={{
-                        width: '100%',
-                        height: '100%',
-                        objectFit: 'cover',
-                    }} />
+        <div className="profile-container">
+            <div className="profile-card">
+                <div className="profile-image-wrapper">
+                    <img src={img} alt={name} className="profile-image" />
                 </div>
-                
+                <div className="profile-details">
+                    <div className="profile-name">{name}</div>
+                    <div className="profile-fullname">{fullname}</div>
+                    <div className="profile-contact">
+                        <FaPhoneAlt className="profile-contact-icon" />
+                        <div className="profile-contact-text">{tel}</div>
+                    </div>
+                    <div className="profile-contact">
+                        <FiCreditCard className="profile-contact-icon" />
+                        <div className="profile-contact-text">
+                            Buyer Credit: {bcredit} | Customer Credit: {ccredit}
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div style={{ flex: '2' }}>
-                <h1> Name: {name}</h1>
-                <h2> Fullname : {fullname}</h2>
-                <h2>Tel: {tel}</h2>
-                <h2>Buyer Credit: {bcredit}</h2>
-                <h2>Customer Credit: {ccredit}</h2>
-            </div>
-            
-            
         </div>
     );
 };
---
+++ ViewerApp/src/components/convertImageToBase64.js
@@ -0,0 +1,49 @@
+﻿function convertImageToBase64(file, callback) {
+    var reader = new FileReader();
+    reader.onload = function () {
+        var dataURL = reader.result;
+        var canvas = document.createElement('canvas');
+        var img = new Image();
+        img.onload = function () {
+            canvas.width = img.width;
+            canvas.height = img.height;
+            var ctx = canvas.getContext('2d');
+            ctx.drawImage(img, 0, 0);
+            var dataURL = canvas.toDataURL('image/jpeg');
+            callback(dataURL);
+        };
+        img.src = dataURL;
+    };
+    reader.readAsDataURL(file);
+}
+
+
+// code html ที่ลองใชัเทส
+
+/*
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Convert Image to Base64 Example</title>
+</head>
+<body>
+    <input type="file" id="input-file">
+    <img id="preview">
+    <div id="result"></div>
+    <script src="convertImageToBase64.js"></script>
+    <script>
+        var input = document.getElementById('input-file');
+        input.addEventListener('change', function() {
+            var file = input.files[0];
+            convertImageToBase64(file, function(base64) {
+                console.log(base64);
+                var img = document.getElementById('preview');
+                img.src = base64;
+                var result = document.getElementById('result');
+                result.innerText = base64;
+            });
+        });
+    </script>
+</body>
+</html>
+*/

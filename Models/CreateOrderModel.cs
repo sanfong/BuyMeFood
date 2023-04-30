e8b41bc4e500fe8ac89517b99e0db21697d83a58
wikimaru <64010892@kmitl.ac.th>
2023-04-30 14:32:58 +0700

    add create order model

---
+++ Models/CreateOrderModel.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace BuyMeFood.Models
+{
+    public class CreateOrderModel
+    {
+        [Required]
+        public int cardID;
+        [Required]
+        public string? storeName { get; set; }
+        [Required]
+        public string? description { get; set; }
+    }
+}

3cc3171b528e60148583a9a96f8ce9906c3db9b5
James <tanakrit.thum@gmail.com>
2023-05-01 00:05:39 +0700

    add AddCard.css, MainHome.css

--- ViewerApp/src/components/MainHome.js
+++ ViewerApp/src/components/MainHome.js
@@ -1,25 +1,27 @@
 ﻿import React, { Component, useState } from 'react';
-import Card from './Card'
+import Card from './Card';
 import AddCard from './AddCard';
+import './MainHome.css';
+
 const MainHome = (props) => {
-    const [displayAdd, setDisplayAdd] = useState(false)
+    const [displayAdd, setDisplayAdd] = useState(false);
+
     return (
         <div>
-          
-            <div style={{ padding: '3rem', borderBottom: '2px solid green', backgroundImage: 'url("https://img.freepik.com/free-vector/hand-drawn-delicious-food-background_52683-16136.jpg?size=626&ext=jpg&ga=GA1.2.284292450.1670951791&semt=sph")', margin: '0' }} className="row">
-                <div class="col-md-6 mx-auto">
+            <div className="custom-row row">
+                <div className="col-md-6 mx-auto">
                     <div className="input-group mb-6" >
                         <input type="text" className="form-control" placeholder="Search Your Location" aria-label="Recipient's username" aria-describedby="button-addon2" />
                         <div className="input-group-append">
-                            <button className="btn btn-danger" type="button" id="button-addon2" style={{ borderBottomLeftRadius: '0', borderTopLeftRadius: '0' }}>Search</button>
+                            <button className="btn btn-danger" type="button" id="button-addon2">Search</button>
                         </div>
                     </div>
                 </div>
             </div>
-            <div className="container mt-4" >
 
+            <div className="container mt-4">
                 <h1>There's something for everyone!</h1>
-                <div className="row justify-content-center" style={{ margin: '0', width: '100%' }}>
+                <div className="row justify-content-center row-custom">
                     {props.data.map(order => {
                         return (
                             <Card item={order} />
@@ -27,12 +29,10 @@ const MainHome = (props) => {
                     })}
                 </div>
             </div>
-            <button onClick={() => { setDisplayAdd(true) }} className='btn btn-success' style={{ position: 'fixed', bottom: '5rem', right: '3rem' }} >+ เพิ่มรายการฝาก</button>
+
+            <button onClick={() => { setDisplayAdd(true) }} className='btn btn-success button-add'>+ เพิ่มรายการฝาก</button>
         </div>
-
-
-    
     )
-
 }
-export default MainHome
+
+export default MainHome;

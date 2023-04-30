c25576d89663e1db5f9824259efc9042f2ca1c50
Kaiizx <atikan9728@gmail.com>
2023-04-30 09:52:09 +0700

    validate singup

--- ViewerApp/src/AppRoutes.js
+++ ViewerApp/src/AppRoutes.js
@@ -4,7 +4,6 @@ import  Home  from "./components/Home";
 import Profile from "./components/Profile";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
-import NewLogin from "./components/NewLogin";
 
 const AppRoutes = [
     {

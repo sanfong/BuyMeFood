ed021daeb2dfe72d7907348cfa937e785f45275c
Apinyawat Khwanpruk <sanfong2222@gmail.com>
2023-05-02 22:18:08 +0700

    Update Authorize

---
+++ Middleware/AuthorizationHandling.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authorization.Policy;
+
+namespace BuyMeFood.Middleware
+{
+    public class AuthorizationHandling : IAuthorizationMiddlewareResultHandler
+    {
+        public async Task HandleAsync(RequestDelegate next, HttpContext context, AuthorizationPolicy policy, PolicyAuthorizationResult authorizeResult)
+        {
+            if (authorizeResult.Challenged)
+            {
+                context.Response.StatusCode = StatusCodes.Status401Unauthorized;
+                await context.Response.WriteAsync("Unauthorized");
+            }
+            else
+            {
+                await next(context);
+            }
+        }
+    }
+}

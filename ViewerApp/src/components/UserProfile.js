a7eea20dca650363548d4c8af7ec82611e4cbe1c
Kaiizx <atikan9728@gmail.com>
2023-05-04 00:32:15 +0700

    edit css edituserproflie

--- ViewerApp/src/components/UserProfile.js
+++ ViewerApp/src/components/UserProfile.js
@@ -1,21 +1,22 @@
 ﻿import Modal from "./Modal";
-
+import './UserProfile.css'
 const UserProfile = (props) => {
     return (
         <Modal onClose={props.onClose}>
         
 
-            <div className=" d-flex justify-content-center" >
+            <div className=" justify-content-center us-container" >
+                <div className="d-flex justify-content-center">
                 <div className="profile-image-wrapper">
 
                     <img src={props.user.image}  className="profile-image" />
-                </div>
+                </div></div>
                 <div className="profile-details">
-                    <div className="profile-name">{props.user.name}</div>
-                    <div className="profile-fullname">{props.user.fullName}</div>
-                    <div className="profile-contact">
+                    <div className="profile-name">Name : {props.user.name}</div>
+                    <div className="profile-name">Full Name : {props.user.fullName}</div>
+                    <div className="profile-name">
   
-                        <div className="profile-contact-text">{props.tel}</div>
+                        <div className="profile-contact-text">Tel : {props.tel}</div>
                     </div>
                     <div className="profile-contact">
 
